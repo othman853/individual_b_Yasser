fede1de4ae2075531728ba2b643249d72a312c93
othman853 <anuar.yasser@gmail.com>
2015-01-28 16:11:21 -0200

    Yasser - Melhorando apresentacao das informações

--- src/br/com/individualc/yasser/CaixaEletronico.java
+++ src/br/com/individualc/yasser/CaixaEletronico.java
@@ -4,25 +4,65 @@ public class CaixaEletronico {
 
 	private static final int[] NOTAS_DISPONIVEIS = { 100, 50, 20, 10 };
 	private static final int MENOR_NOTA = 10;
+	
+	private int contadorNotasDe10 = 0;
+	private int contadorNotasDe20 = 0;
+	private int contadorNotasDe50 = 0;
+	private int contadorNotasDe100 = 0;
 
-	public void sacar(double valor) {
+	public void sacar(double valorDoSaque) {
 		
-		while (valor >= MENOR_NOTA) {
+		zerarContagemDeNotas();
+		int nota = 0;
+		while (valorDoSaque >= MENOR_NOTA) {
+			
 			for (int i = 0; i < NOTAS_DISPONIVEIS.length; i++) {
-				if (valor >= NOTAS_DISPONIVEIS[i]) {
-					
-					valor -= NOTAS_DISPONIVEIS[i];					
-					if (valor > 0 && valor < MENOR_NOTA) {
+				nota = NOTAS_DISPONIVEIS[i];
+				if (valorDoSaque >= nota) {					
+					valorDoSaque -= nota;					
+					if (valorDoSaque > 0 && valorDoSaque < MENOR_NOTA) {
 						System.out.println("VALOR INVALIDO");
 						return;
 					}
 					
-					System.out.println("NOTA: " + NOTAS_DISPONIVEIS[i]);
+					contabilizarNota(nota);					
 					i--;
 					continue;
-				}
-				
-			}
-		}		
+				}				
+			}			
+		}
+		
+		imprimirNotasSacadas();
+	}
+	
+	private void zerarContagemDeNotas(){
+		contadorNotasDe10 = 0;
+		contadorNotasDe20 = 0;
+		contadorNotasDe50 = 0;
+		contadorNotasDe100 = 0;		
+	}
+	
+	private void contabilizarNota(int valor){
+		switch(valor){
+		case 10:
+			contadorNotasDe10++;
+			break;
+		case 20:
+			contadorNotasDe20++;
+			break;
+		case 50:
+			contadorNotasDe50++;
+			break;
+		case 100:
+			contadorNotasDe100++;
+			break;
+		}
+	}
+	
+	public void imprimirNotasSacadas(){
+		System.out.println("[10]  : " + contadorNotasDe10);
+		System.out.println("[20]  : " + contadorNotasDe20);
+		System.out.println("[50]  : " + contadorNotasDe50);
+		System.out.println("[100] : " + contadorNotasDe100);
 	}
 }
--- src/br/com/individualc/yasser/Principal.java
+++ src/br/com/individualc/yasser/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		CaixaEletronico caixa = new CaixaEletronico();
 		
-		caixa.sacar(30);
+		caixa.sacar(180);
 	}
 
 }
